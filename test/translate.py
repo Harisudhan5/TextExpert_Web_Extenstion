2d589b8a3981a8dad2ce9b9c896cffda3e1739c7
Harisudhan5 <speaktoharisudhan@gmail.com>
2024-05-26 11:18:44 +0530

    testing translation

---
+++ test/translate.py
@@ -0,0 +1,10 @@
+from googletrans import Translator, constants
+
+translator = Translator()
+text = "Hi how are you"
+dest_lang = 'es'
+translation = translator.translate(text, dest=dest_lang)
+
+print (translation.text)
+print(translation.origin)
+print(translation.dest)
