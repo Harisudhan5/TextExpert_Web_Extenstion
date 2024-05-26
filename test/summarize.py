0cdfa2c4d948a6ca82a842043f989034ebcc6e4f
Harisudhan5 <speaktoharisudhan@gmail.com>
2024-05-26 19:13:49 +0530

    fixed summarize function

--- test/summarize.py
+++ test/summarize.py
@@ -1,28 +1,8 @@
-from groq import Groq
+import google.generativeai as genai
+api_key = ""
+genai.configure(api_key = api_key)
 
-def groq_completion(user_content):
-    client = Groq(api_key = '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')
-    completion = client.chat.completions.create(
-        model="mixtral-8x7b-32768",
-        messages=[
-            {
-                "role": "system",
-                "content": "summarize the content with the specified content"
-            },
-            {
-                "role": "user",
-                "content": user_content
-            }
-        ],
-        temperature=0.5,
-        max_tokens=56400,
-        top_p=1,
-        stream=True,
-        stop=None,
-    )
+model = genai.GenerativeModel('gemini-1.0-pro-latest')
+response = model.generate_content("The opposite of hot is")
+print(response.text)
 
-
-    result = ''
-    for chunk in completion:
-        result += chunk.choices[0].delta.content or ""
-    return result
